fda33a2e46d525b40d376ec0e6d9b9ad877e6582
infy <infykun@gmail.com>
2020-04-14 18:47:33 +0300

    5.7. Ипотека.[#265280]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        double p = amount;
+        while(p > 0) {
+            p = p * percent/100 + p - salary;
+            year++;
+        }
+        return year;
+    }
+}
