6216481e0399d751fc139f126974875568ff9dd8
infy <infykun@gmail.com>
2020-04-19 21:03:36 +0300

    6.2.3. Перевернуть массив.[#265228]

--- test/ru/job4j/array/TurnTest.java
+++ test/ru/job4j/array/TurnTest.java
@@ -8,9 +8,26 @@ public class TurnTest {
     @Test
     public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
         Turn turner = new Turn();
-        int[] input = new int[]{4, 1, 6, 2};
+        int[] input = new int[] {4, 1, 6, 2};
         int[] result = turner.back(input);
-        int[] expect = new int[]{2, 6, 1, 4};
+        int[] expect = new int[] {2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray1() {
+        Turn turner = new Turn();
+        int[] input = new int[] {8, 5, 7, 3, 54, 2, 5};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {5, 2, 54, 3, 7, 5, 8};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray2() {
+        Turn turner = new Turn();
+        int[] input = new int[] {3, 6, 2, 5, 7, 8};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {8, 7, 5, 2, 6, 3};
         assertThat(result, is(expect));
     }
 }
