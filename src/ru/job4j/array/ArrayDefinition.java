e96564c5291c33636b467acabdd2d41a99200639
infy <infykun@gmail.com>
2020-04-19 20:00:12 +0300

    6.0.2. Заполнение массива.[#265225]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Misha Pipka";
+        names[1] = "Slava Yoshin";
+        names[2] = "Alexy Layho";
+        names[3] = "Chuck Norris";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
