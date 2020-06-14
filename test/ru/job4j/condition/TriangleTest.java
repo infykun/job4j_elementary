1590d15f3f03b2857717874f0e4e20525aa4ac85
infy <infykun@gmail.com>
2020-06-14 19:46:10 +0300

    2.2. Рефакторинг - Площадь треугольника. [#265364]

--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -7,22 +8,38 @@ import static org.junit.Assert.assertThat;
 public class TriangleTest {
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+        Point ap = new Point(1, 2);
+        Point bp = new Point(2, 2);
+        Point cp = new Point(2, 3);
+        Triangle triangle = new Triangle(ap, bp, cp);
+        double result = triangle.area();
+        Assert.assertEquals(0.5, result, 0.01);
     }
     @Test
     public void whenExist1() {
-        boolean result = Triangle.exist(3.0, 2.0, 2.0);
-        assertThat(result, is(true));
+        Point ap = new Point(1, 2);
+        Point bp = new Point(2, 2);
+        Point cp = new Point(2, 5);
+        Triangle triangle = new Triangle(ap, bp, cp);
+        double result = triangle.area();
+        Assert.assertEquals(1.5, result, 0.01);
     }
     @Test
     public void whenExist2() {
-        boolean result = Triangle.exist(2.0, 4.0, 2.0);
-        assertThat(result, is(false));
+        Point ap = new Point(1, 2);
+        Point bp = new Point(2, 2);
+        Point cp = new Point(3, 2);
+        Triangle triangle = new Triangle(ap, bp, cp);
+        double result = triangle.area();
+        Assert.assertEquals(-1, result, 0.01);
     }
     @Test
     public void whenExist3() {
-        boolean result = Triangle.exist(2.0, 2.0, 8.0);
-        assertThat(result, is(false));
+        Point ap = new Point(1, 2);
+        Point bp = new Point(2, 20);
+        Point cp = new Point(20, 3);
+        Triangle triangle = new Triangle(ap, bp, cp);
+        double result = triangle.area();
+        Assert.assertEquals(170.5, result, 0.01);
     }
 }
