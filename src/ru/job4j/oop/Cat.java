d41fe92a02cc00fa35e81c36bdd3dc0126b7d10b
infy <infykun@gmail.com>
2020-06-14 13:01:32 +0300

    1.5. Поля объекта [#265368]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -1,7 +1,25 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+    public void eat(String meat) {
+        this.food = meat;
+    }
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
     public static void main(String[] args) {
-        Cat peppy = new Cat();
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Gav");
+        gav.show();
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Black");
+        black.show();
     }
 }
