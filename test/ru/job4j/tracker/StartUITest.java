66637e072f632e1f08ea0eb039c95d39d76d32f0
infy <infykun@gmail.com>
2021-01-24 10:05:36 +0300

    1.1. Тесты на ValidateInput. [#265357]

--- test/ru/job4j/tracker/StartUITest.java
+++ test/ru/job4j/tracker/StartUITest.java
@@ -8,6 +8,27 @@ import static org.junit.Assert.assertThat;
 public class StartUITest {
 
     @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"2", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit program%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit program%n"
+                )
+        ));
+    }
+    @Test
     public void whenExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
